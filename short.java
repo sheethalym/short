7c9c33534da7c714fb125cebbc3b39ca7ff909a6
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:38:36 +0530

    adding short program

---
+++ short.java
@@ -0,0 +1,17 @@
+class Short{
+ public static void main(String[]args){
+  short[] year={1990,1991,1992,1993,1994,1995,1996,1997,1998,1999};
+  System.out.println(year[0]);
+  System.out.println(year[1]);
+  System.out.println(year[2]);
+  System.out.println(year[3]);
+  System.out.println(year[4]);
+  System.out.println(year[5]);
+  System.out.println(year[6]);
+  System.out.println(year[7]);
+  System.out.println(year[8]);
+  System.out.println(year[9]);
+
+ }
+
+}
